1edd87e6d40034e329de4b15466f37e4a337d1f7
jonasxwebdev <info@jonasreidel.de>
2024-08-16 10:05:56 +0200

    new packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+	compatibilityDate: "2024-04-03",
+	image: {
+		domains: ["plant.id"],
+	},
+
+	devtools: {
+		enabled: true,
+
+		timeline: {
+			enabled: true,
+		},
+	},
+
+	css: ["~/assets/css/main.css"],
+
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+
+	runtimeConfig: {
+		// Make environment variables accessible here
+		public: {
+			VITE_PLANTID_API_KEY: process.env.VITE_PLANTID_API_KEY,
+			// Add more environment variables as needed
+		},
+	},
+
+	modules: ["@nuxt/image"],
+});
